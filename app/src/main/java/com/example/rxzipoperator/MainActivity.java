da2c8592c9caeb734361a3fa61faa732d472e13a
sourcecode121 <sourcecode121@users.noreply.github.com>
2016-09-26 20:01:55 +0530

    Adding the unsubscribe method

--- app/src/main/java/com/example/rxzipoperator/MainActivity.java
+++ app/src/main/java/com/example/rxzipoperator/MainActivity.java
@@ -13,6 +13,7 @@ import java.util.UUID;
 import rx.Observable;
 import rx.Scheduler;
 import rx.Subscriber;
+import rx.Subscription;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Func0;
 import rx.functions.Func2;
@@ -20,12 +21,14 @@ import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Subscription subscription;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Observable.zip(getCustomer(), getOrder(), new Func2<Customer, Order, CustomerOrderInfo>() {
+        subscription = Observable.zip(getCustomer(), getOrder(), new Func2<Customer, Order, CustomerOrderInfo>() {
             @Override
             public CustomerOrderInfo call(Customer customer, Order order) {
                 return new CustomerOrderInfo(customer.getId(), order.getId());
@@ -49,6 +52,12 @@ public class MainActivity extends AppCompatActivity {
                 Log.d("Rx", "Customer Info");
             }
         });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        subscription.unsubscribe();
     }
 
     public Observable<Customer> getCustomer(){
